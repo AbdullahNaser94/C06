921f509d5f20627488c8c0c223f78075b415be94
AbdullahNaser94 <abdulahnaser94@gmail.com>
2024-06-19 20:41:01 +0300

    solve C06

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,24 @@
+// #include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int main(int argc, char *argv[])
+{
+	int	j;
+
+	j = 0;
+	if (argc > 0)
+	{
+		while (argv[0][j] != '\0')
+		{
+			ft_putchar(argv[0][j]);
+			j++;
+		}
+		ft_putchar('\n');
+	}
+	return (0);
+}
